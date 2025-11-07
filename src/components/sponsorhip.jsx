51fcf841c28d7a55488b189c9e68b969de344331
Gabriel W Kun <gabrielwkun@Gabriels-MacBook-Pro.local>
2025-11-07 13:41:30 +0000

    test 6

---
+++ src/components/sponsorhip.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Sponsorhip extends React.Component{
+
+    render (){
+
+        return (
+
+            <>
+            
+            <div className="h-96 bg-white">
+                <h1 className="font-sans font-bold uppercase text-3xl text-center">
+                    partners
+                </h1>
+            </div>
+            </>
+        )
+    }
+}
+
+export default Sponsorhip;
